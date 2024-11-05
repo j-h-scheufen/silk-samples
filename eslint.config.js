ccfea9b5bfc3a546abf6495980310af22896af95
Jan Hendrik Scheufen <j.h.scheufen@gmail.com>
2024-11-05 11:17:45 -0500

    build: added app/react-app
    Using eslint 10 experimental feature --flag unstable_config_lookup_from_file to detect
    multiple eslint configs across packages
    
    Signed-off-by: Jan Hendrik Scheufen <j.h.scheufen@gmail.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,17 @@
 import globals from 'globals';
-import pluginJs from '@eslint/js';
+import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
+import react from 'eslint-plugin-react';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
   { languageOptions: { globals: globals.browser } },
-  { settings: { react: { version: 'detect' } } },
-  pluginJs.configs.recommended,
+  { settings: { react: { version: '18.3.1' } } },
+  js.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  react.configs.flat.recommended,
   eslintPluginPrettierRecommended,
   {
     ignores: [
