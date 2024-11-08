f514fe3dd2707795cc42fd92b2c7acb7b3e12828
Jan Hendrik Scheufen <j.h.scheufen@gmail.com>
2024-11-08 12:39:16 -0500

    fix: added missing vite.config for index files
    Signed-off-by: Jan Hendrik Scheufen <j.h.scheufen@gmail.com>

--- packages/common/vite.config.ts
+++ packages/common/vite.config.ts
@@ -5,7 +5,12 @@ import dts from 'vite-plugin-dts';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    lib: { entry: resolve(__dirname, 'src/index.ts'), formats: ['es'] },
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es', 'cjs'],
+      name: 'Silk Samples Common',
+      fileName: 'index',
+    },
   },
   plugins: [dts()],
 });
